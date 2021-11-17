f6e9da96c43e1e9e28014fdbd74f3cc89043bf72
alex <skisk8fmx@gmail.com>
2021-11-17 18:01:27 +0300

    component and functions

--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import UserList from "./components/User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            'users':[]
+        }
+    }
+
+    componentDidMount() {
+        const users = [
+            {
+                'username': 'Alex',
+                'first_name': 'Alexander',
+                'last_name': 'Naumov',
+                'email': 'AlexanderNaumov@TODO.com'
+            }
+        ]
+        this.setState(
+            {
+                'users':users
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    List of users
+                </header>
+
+                <div className="App-content">
+                    <UserList users={this.state.users}/>
+                </div>
+                <footer>
+                    <p>TODO application</p>
+                </footer>
+            </div>
+
+        )
+            ;
+    }
 }
 
 export default App;
