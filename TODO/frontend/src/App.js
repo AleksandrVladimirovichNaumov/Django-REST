636e19a606c2f6b78c9cbfcdae2eb9bb8be19f88
alex <skisk8fmx@gmail.com>
2021-11-18 20:11:46 +0300

    CORS

--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import './App.css';
 import UserList from "./components/User";
+import axios from "axios";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            'users':[]
+        this.state = {
+            'users': []
         }
     }
 
     componentDidMount() {
-        const users = [
-            {
-                'username': 'Alex',
-                'first_name': 'Alexander',
-                'last_name': 'Naumov',
-                'email': 'AlexanderNaumov@TODO.com'
+        axios.get('http://127.0.0.1:8000/api/users/').then(
+            response => {
+                const users = response.data
+                console.log(users)
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
             }
-        ]
-        this.setState(
-            {
-                'users':users
-            }
-        )
+        ).catch(error => console.log(error))
+        // const users = [
+        //     {
+        //         'username': 'Alex',
+        //         'first_name': 'Alexander',
+        //         'last_name': 'Naumov',
+        //         'email': 'AlexanderNaumov@TODO.com'
+        //     }
+        // ]
+        // this.setState(
+        //     {
+        //         'users':users
+        //     }
+        // )
     }
 
     render() {
