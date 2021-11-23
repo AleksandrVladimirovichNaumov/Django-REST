fa74b494b682127005f80e43024200d40991f2d2
alex <skisk8fmx@gmail.com>
2021-11-23 14:27:21 +0300

    API todos and projects added

--- TODO/projects/models.py
+++ TODO/projects/models.py
@@ -9,15 +9,18 @@ from users.models import User
 class Project(models.Model):
     name = models.CharField(max_length=50)
     git_link = models.URLField(unique=True)
-    working_group = models.ManyToManyField(User)
+    working_group = models.ManyToManyField(User, verbose_name='working_group')
+
+    def __str__(self):
+        return self.name
 
 
 class ToDo(models.Model):
     name = models.CharField(max_length=50)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project')
     description = models.CharField(max_length=250)
     create_datetime = models.DateTimeField(default=now, editable=False)
     update_datetime = models.DateTimeField(default=now)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_to')
     is_active = models.BooleanField(default=True)
