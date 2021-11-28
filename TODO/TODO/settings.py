8df312791fbd97c204f22d7e3b09f02f86befdf3
alex <skisk8fmx@gmail.com>
2021-11-28 13:31:22 +0300

    PAGINATION = 100

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -135,7 +135,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 if DEBUG:
