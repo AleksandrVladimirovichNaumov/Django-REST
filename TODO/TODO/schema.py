1b1036654a8392d046f802acd5b48a9e0933bd9c
alex <skisk8fmx@gmail.com>
2021-12-15 18:40:57 +0300

    project_by_todo added

--- TODO/TODO/schema.py
+++ TODO/TODO/schema.py
@@ -7,17 +7,19 @@ from users.models import User
 
 class ProjectType(DjangoObjectType):
     class Meta:
-        model= Project
-        fields='__all__'
+        model = Project
+        fields = '__all__'
+
 
 class ToDoType(DjangoObjectType):
     class Meta:
-        model= ToDo
-        fields='__all__'
+        model = ToDo
+        fields = '__all__'
+
 
 class UserType(DjangoObjectType):
     class Meta:
-        model= User
+        model = User
         fields = '__all__'
 
 
@@ -25,6 +27,7 @@ class Query(graphene.ObjectType):
     all_projects = graphene.List(ProjectType)
     all_todos = graphene.List(ToDoType)
     all_users = graphene.List(UserType)
+    project_by_todo = graphene.List(ToDoType, todo_id=graphene.Int(required=False))
 
     def resolve_all_todos(root, info):
         return ToDo.objects.all()
@@ -35,7 +38,34 @@ class Query(graphene.ObjectType):
     def resolve_all_users(root, info):
         return User.objects.all()
 
+    def resolve_project_by_todo(self, info, todo_id=None):
+        """
+        to test okease send graphqi request below
 
+        {
+          projectByTodo(todoId:1) {
+            id
+            name
+            isActive
+            createdBy{
+              username
+            }
+            createDatetime
+            updateDatetime
+            project{
+              id
+              name
+              workingGroup{
+                username
+              }
+            }
+          }
+        }
+        """
+        if todo_id:
+            return ToDo.objects.filter(id=todo_id)
+        else:
+            return ToDo.objects.all()
 
 
 schema = graphene.Schema(query=Query)
